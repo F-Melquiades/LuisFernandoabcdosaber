c138fef4ae6d9f03fda70014fec931b95c1b5162
Fernando Melquiades <fmelquiades5@gmail.com>
2024-08-22 21:40:33 -0300

    Base de Dados

--- aluno/admin.py
+++ aluno/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
+from aluno.models import Aluno
+
 # Register your models here.
+admin.site.register(Aluno)
--- aluno/models.py
+++ aluno/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Aluno(models.Model):
+    """ Modelo representando um Aluno """
+    matricula = models.AutoField(primary_key=True, null=False, help_text='Matrícula do Aluno')
+    nome = models.CharField(max_length=70, null=False, help_text='Informe o nome do Aluno')
+    dataMatricula = models.DateField(max_length=70, null=False, help_text='Informe a Data da Matrícula do Aluno')
+    dataSaida = models.DateField(max_length=70, null=True, blank=True, help_text='Informe a Data de Saída do Aluno')
+    
+    def __str__(self):
+        return f'{self.matricula} {self.nome} {self.dataMatricula} {self.dataSaida}'
+    
+    def get_absolute_url(self):
+        return reverse("model_detail", kwargs={"pk": self.pk})
