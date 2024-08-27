b751f6bf8a7c2fcaadcfcac79a9de8313a7debad
Fernando Melquiades <fmelquiades5@gmail.com>
2024-08-27 20:38:15 -0300

    Trabalho Final

--- titulo/views.py
+++ titulo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from titulo.models import Titulo
+from titulo.forms import TituloForm
 
 def cadastrar(request):
     return render(request, 'titulo/cadastroTitulos.html')
@@ -11,3 +12,15 @@ def listar(request):
     contexto = {'tipos_titulo_lista': registros}
     
     return render(request, 'titulo/listarTitulos.html', contexto)
+
+def cadastro(request):
+    
+    form = TituloForm(request.POST)
+    if form.is_valid():
+        dados_titulo = form.cleaned_data
+        titulo = Titulo(
+            descricao = dados_titulo['descricao']
+        )
+        titulo.save()
+        
+    return render(request, 'titulo/cadastroTitulos.html')
